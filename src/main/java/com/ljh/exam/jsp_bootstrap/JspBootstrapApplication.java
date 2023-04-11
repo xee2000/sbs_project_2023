b03c48301412842b8030c82a6aca84e3858246f9
xee2000 <xee2000@naver.com>
2023-04-11 18:16:17 +0900

    첫커밋

---
+++ src/main/java/com/ljh/exam/jsp_bootstrap/JspBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.ljh.exam.jsp_bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspBootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljh/exam/jsp_bootstrap/JspBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljh.exam.jsp_bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

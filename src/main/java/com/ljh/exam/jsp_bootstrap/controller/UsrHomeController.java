97317cb6d795c33de8dc90dbb69fbeeaf3001361
xee2000 <xee2000@naver.com>
2023-04-13 15:56:02 +0900

    자동증가 액션

--- src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrHomeController.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrHomeController.java
@@ -7,10 +7,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 
 public class UsrHomeController {
-
+	private int count;
 	@RequestMapping("/usr/home/main")
 	@ResponseBody
 	public String showMain() {
 		return "안녕하세요";
 	}
+	@RequestMapping("/usr/home/main2")
+	@ResponseBody
+	public String showMain2() {
+		return "반갑습니다.";
+	}
+	@RequestMapping("/usr/home/main3")
+	@ResponseBody
+	public String showMain3() {
+		return "또 만나요";
+		
+	}
+	@RequestMapping("/usr/home/main4")
+	@ResponseBody
+	public int showMain4() {
+	count++;
+		return count;
+	}
+	@RequestMapping("/usr/home/main5")
+	@ResponseBody
+	public String showMain5() {
+	count = 0;
+		return "count의 값이 0으로 초기화 되었습니다.";
+	}
+
+	
 }
+
+

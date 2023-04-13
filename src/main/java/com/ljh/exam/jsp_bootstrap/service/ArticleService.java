82d13559edc690593adba9603d22db0c7783a89a
xee2000 <xee2000@naver.com>
2023-04-13 18:39:04 +0900

    서비스 repository controller정리

---
+++ src/main/java/com/ljh/exam/jsp_bootstrap/service/ArticleService.java
@@ -0,0 +1,37 @@
+package com.ljh.exam.jsp_bootstrap.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ljh.exam.jsp_bootstrap.repository.ArticleRepository;
+import com.ljh.exam.jsp_bootstrap.vo.Article;
+@Service
+public class ArticleService {
+	private ArticleRepository articleRepository;
+	
+	public ArticleService(ArticleRepository articleRepository) {
+		this.articleRepository = articleRepository;
+		articleRepository.makeTestData();
+	}
+	
+	public Article getArticle(int id) {
+		return articleRepository.getArticle(id);
+	}
+	
+	public List<Article> getArticles() {
+		return articleRepository.getArticles();
+	}
+	
+	public Article writeArticle(String title, String body) {
+		return articleRepository.writeArticle(title, body);
+	}
+	
+	public void deleteArticle(int id) {
+		articleRepository.deleteArticle(id);
+	}
+
+	public void modifyArticle(int id, String title, String body) {
+		articleRepository.modifyArticle(id, title, body);
+	}
+}

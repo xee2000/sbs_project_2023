f27a9e272c9f2443b3268940c8909bd02b4ee6a0
xee2000 <xee2000@naver.com>
2023-04-14 17:44:18 +0900

    DELETE, UPDATE, SELECT관련 쿼리를 메서드와 연결

--- src/main/java/com/ljh/exam/jsp_bootstrap/repository/ArticleRepository.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/repository/ArticleRepository.java
@@ -2,21 +2,30 @@ package com.ljh.exam.jsp_bootstrap.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.ljh.exam.jsp_bootstrap.vo.Article;
 
 @Mapper
 public interface ArticleRepository {
-
-	public Article getArticle(int id);
-
+	
+	@Select("SELECT * FROM article WHERE ID = #{id}")
+	public Article getArticle(@Param("id") int id);
+	
+	@Select("SELECT * FROM article ORDER BY ID DESC")
 	public List<Article> getArticles();
 
+	
 	public Article writeArticle(String title, String body);
+	
+	@Delete("DELETE FROM article WHERE ID = #{id}")
+	public void deleteArticle(@Param("id")int id);
 
-	public void deleteArticle(int id);
-
-	public void modifyArticle(int id, String title, String body);
+	@Update("UPDATE article SET title = #{title} , `body` = #{body}, updateDate = NOW WHERE ID = #{id}")
+	public void modifyArticle(@Param("id")int id, @Param("title")String title,@Param("body") String body);
 
 }

c35f39c88cfb9bec70ff9139e6fc635fb56bd112
xee2000 <xee2000@naver.com>
2023-04-17 17:46:02 +0900

    코드중복 제거

--- src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrMemberController.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrMemberController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ljh.exam.jsp_bootstrap.service.MemberService;
+import com.ljh.exam.jsp_bootstrap.utill.Ut;
 import com.ljh.exam.jsp_bootstrap.vo.Member;
 
 @Controller
@@ -25,22 +26,22 @@ public class UsrMemberController {
 	 Object dojoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
 			String email) {
 		
-	if(loginId == null || loginId.trim().length() == 0) {
+	if(Ut.empty(loginId)) {
 		return "loginId를(을) 입력해주세요.";
 	}
-	if(loginPw == null || loginPw.trim().length() == 0) {
+	if(Ut.empty(loginPw)) {
 		return "loginPw를(을) 입력해주세요.";
 	}
-	if(name == null || name.trim().length() == 0) {
+	if(Ut.empty(name)) {
 		return "name를(을) 입력해주세요.";
 	}
-	if(nickname == null || nickname.trim().length() == 0) {
+	if(Ut.empty(nickname)) {
 		return "nickname를(을) 입력해주세요.";
 	}
-	if(cellphoneNo == null || cellphoneNo.trim().length() == 0) {
+	if(Ut.empty(cellphoneNo)) {
 		return "cellphoneNo를(을) 입력해주세요.";
 	}
-	if(email == null || email.trim().length() == 0) {
+	if(Ut.empty(email)) {
 		return "email를(을) 입력해주세요.";
 	}
 	int id = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
---
+++ src/main/java/com/ljh/exam/jsp_bootstrap/utill/Ut.java
@@ -0,0 +1,16 @@
+package com.ljh.exam.jsp_bootstrap.utill;
+
+public class Ut {
+
+	public static boolean empty(Object obj) {
+		if (obj == null) {
+			return true;
+		}
+		if (obj instanceof String == false) {
+			return true;
+		}
+		String str = (String) obj;
+		return str.trim().length() == 0;
+
+	}
+}

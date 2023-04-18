1cdf36b7b367c875927575a2a190b0c6a41a7945
xee2000 <xee2000@naver.com>
2023-04-18 18:52:05 +0900

    기능추가

--- src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrArticleController.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrArticleController.java
@@ -50,7 +50,7 @@ public class UsrArticleController {
 		
 		Article article = articleService.getArticle(id);
 		
-		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), article);
+		return ResultData.from(writeArticleRd.getResultCode(), writeArticleRd.getMsg(), "article", article);
 	}
 	
 	@RequestMapping("/usr/article/getArticles")
@@ -58,7 +58,7 @@ public class UsrArticleController {
 	public ResultData<List<Article>> getArticles() {
 		List<Article> articles = articleService.getArticles();
 		
-		return ResultData.from("S-1", "게시물 리스트입니다.", articles);
+		return ResultData.from("S-1", "게시물 리스트입니다.", "articles", articles);
 	}
 	
 	@RequestMapping("/usr/article/getArticle")
@@ -70,7 +70,7 @@ public class UsrArticleController {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
-		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id), article);
+		return ResultData.from("S-1", Ut.f("%d번 게시물입니다.", id),"article", article);
 	}
 	
 	@RequestMapping("/usr/article/doDelete")
@@ -92,9 +92,10 @@ public class UsrArticleController {
 		
 		Article article = articleService.getArticle(id);
 		
-		if(article.getMemberId() != loginedMemberId) {
-			return ResultData.from("F-2", "로그인 후 이용해주세요.");
+		if ( article.getMemberId() != loginedMemberId) {
+			return ResultData.from("F-2","권한이 없습니다.");
 		}
+		
 		
 		if ( article == null ) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
@@ -102,21 +103,37 @@ public class UsrArticleController {
 		
 		articleService.deleteArticle(id);
 		
-		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id), id);
+		return ResultData.from("S-1", Ut.f("%d번 게시물을 삭제하였습니다.", id),"id", id);
 	}
 	
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public ResultData<Integer> doModify(int id, String title, String body) {
+	public ResultData<Article> doModify(int id, String title, String body, HttpSession httpSession) {
+		
+		boolean isLogined = false;
+		int loginedMemberId = 0;
+		
+		if ( httpSession.getAttribute("loginedMemberId") != null) {
+			isLogined = true;
+			loginedMemberId = (int) httpSession.getAttribute("loginedMemberId");
+		}
+		
+		if ( isLogined == false ) {
+			return ResultData.from("F-A", "로그인 후 이용해주세요.");
+		}
 		Article article = articleService.getArticle(id);
 		
-		if ( article == null ) {
+		if(article == null) {
 			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
 		
 		articleService.modifyArticle(id, title, body);
+		ResultData actorCanModifyRd = articleService.actorCanModify(loginedMemberId, article);
+		if(actorCanModifyRd.isFail()) {
+			return actorCanModifyRd;
+		}
 		
-		return ResultData.from("S-1", Ut.f("%d번 게시물을 수정하였습니다.", id), id);
+		return articleService.modifyArticle(id, title, body);
 	}
 	// 액션 메서드 끝
 }
--- src/main/java/com/ljh/exam/jsp_bootstrap/vo/ResultData.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/vo/ResultData.java
@@ -8,26 +8,30 @@ public class ResultData<DT> {
 	@Getter
 	private String msg;
 	@Getter
+	private String data1Name;
+	@Getter
 	private DT data1;
 	
 	private ResultData() {
 		
 	}
+	
 	public static ResultData from(String resultCode, String msg) {
-		return from(resultCode,msg,null);
+		return from(resultCode, msg, null, null);
 	}
 	
-	public static <DT>ResultData<DT> from(String resultCode, String msg, DT data1) {
+	public static <DT> ResultData<DT> from(String resultCode, String msg, String data1Name, DT data1) {
 		ResultData<DT> rd = new ResultData<DT>();
 		rd.resultCode = resultCode;
 		rd.msg = msg;
+		rd.data1Name = data1Name;
 		rd.data1 = data1;
 		
 		return rd;
 	}
-	public static <DT> ResultData<DT> newData(ResultData joinRd, DT newData) {
-		return from(joinRd.getResultCode(),joinRd.getMsg(), newData);
-		
+	
+	public static <DT> ResultData<DT> newData(ResultData oldRd, String data1Name, DT data1) {
+		return from(oldRd.getResultCode(), oldRd.getMsg(), data1Name, data1);
 	}
 	
 	public boolean isSuccess() {
@@ -37,5 +41,9 @@ public class ResultData<DT> {
 	public boolean isFail() {
 		return isSuccess() == false;
 	}
+
+	
+
+	
 
 }

53b9648cdcf397ee6d1d8d04a179661192a169a2
xee2000 <xee2000@naver.com>
2023-04-21 15:15:27 +0900

    로그아웃 후 자바스크립트로 후 처리

--- src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrMemberController.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrMemberController.java
@@ -67,18 +67,18 @@ public class UsrMemberController {
 	
 	@RequestMapping("/usr/member/doLogout")
 	@ResponseBody
-	public ResultData<Member> doLogout(HttpServletRequest req) {
+	public String doLogout(HttpServletRequest req) {
 		Rq rq = (Rq) req.getAttribute("rq");
-		if ( rq.isLogined() ) {
-			return ResultData.from("S-1", "이미 로그아웃 상태입니다.");
+		if (!rq.isLogined() ) {
+			return Ut.jsHistoryBack("이미 로그아웃 상태입니다.");
 		}
 		
 		rq.logout();
-		return ResultData.from("S-2", "로그아웃 되었습니다.");
+		return Ut.jsReplace("로그아웃 되었습니다.","/");
 	}
 	
 	@RequestMapping("/usr/member/Login")
-	public String showLogin(HttpSession httpSession) {
+	public String showLogin() {
 		return "usr/member/login";
 	}
 	

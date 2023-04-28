b8245230112e43938be677a80efaa28df648c8e8
xee2000 <xee2000@naver.com>
2023-04-28 18:02:35 +0900

    attr 테이블 추가

--- db/schema.sql
+++ db/schema.sql
@@ -263,3 +263,27 @@ SELECT A.*,
 			where R.relTypeCode = #{relTypeCode}
 			and R.relId = #{relId}
 			order by R.id desc
+
+			#부가정보테이블 추가
+		CREATE TABLE attr(
+		id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+		regDate DATETIME NOT NULL,
+		updateDate DATETIME NOT NULL,
+		`relTypeCode` CHAR(20) NOT NULL,
+		`relId` INT(10) UNSIGNED NOT NULL,
+		`typeCode` CHAR(30) NOT NULL,
+		`type2Code` CHAR(70) NOT NULL,
+		`value` TEXT NOT NULL
+		);
+		
+		#attr 유니크 인덱스 걸기
+		##중복변수 생성금지
+		##변수 찾는 속도 최적화
+		ALTER TABLE `attr` ADD UNIQUE INDEX(`relTypeCode`,`relId`,`typeCode`,`type2Code`);
+	
+		## 특정 조건을 만족하는 회원 또는 게시물(기타 데이터)를 빠르게 찾기 위해서
+		ALTER TABLE `attr` ADD INDEX(`relTypeCode`,`typeCode`,`type2Code`);
+		##attr에 만료 날짜 추가
+		ALTER TABLE `attr` ADD COLUMN `expireData` DATETIME NULL AFTER `value`;
+		DESC attr;
+		
--- src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrMemberController.java
+++ src/main/java/com/ljh/exam/jsp_bootstrap/controller/UsrMemberController.java
@@ -20,154 +20,156 @@ import jakarta.servlet.http.HttpSession;
 public class UsrMemberController {
 	private MemberService memberService;
 	private Rq rq;
-
+	
 	public UsrMemberController(MemberService memberService, Rq rq) {
 		this.memberService = memberService;
 		this.rq = rq;
 	}
-
+	
 	@RequestMapping("/usr/member/doJoin")
 	@ResponseBody
-	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo,
-			String email) {
-		if (Ut.empty(loginId)) {
+	public ResultData<Member> doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		if ( Ut.empty(loginId) ) {
 			return ResultData.from("F-1", "loginId(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(loginPw)) {
+		
+		if ( Ut.empty(loginPw) ) {
 			return ResultData.from("F-2", "loginPw(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(name)) {
+		
+		if ( Ut.empty(name) ) {
 			return ResultData.from("F-3", "name(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(nickname)) {
+		
+		if ( Ut.empty(nickname) ) {
 			return ResultData.from("F-4", "nickname(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(cellphoneNo)) {
+		
+		if ( Ut.empty(cellphoneNo) ) {
 			return ResultData.from("F-5", "cellphoneNo(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(email)) {
+		
+		if ( Ut.empty(email) ) {
 			return ResultData.from("F-6", "email(을)를 입력해주세요.");
 		}
-
+		
 		// S-1
 		// 회원가입이 완료되었습니다.
 		// 7
 		ResultData<Integer> joinRd = memberService.join(loginId, loginPw, name, nickname, cellphoneNo, email);
-
-		if (joinRd.isFail()) {
-			return (ResultData) joinRd;
+		
+		if ( joinRd.isFail() ) {
+			return (ResultData)joinRd;
 		}
-
+		
 		Member member = memberService.getMemberById(joinRd.getData1());
-
+		
 		return ResultData.newData(joinRd, "member", member);
 	}
-
+	
 	@RequestMapping("/usr/member/doLogout")
 	@ResponseBody
 	public String doLogout() {
-		if (!rq.isLogined()) {
+		if ( !rq.isLogined() ) {
 			return rq.jsHistoryBack("이미 로그아웃 상태입니다.");
 		}
-
+		
 		rq.logout();
-
+		
 		return rq.jsReplace("로그아웃 되었습니다.", "/");
 	}
-
+	
 	@RequestMapping("/usr/member/login")
 	public String showLogin() {
 		return "usr/member/login";
 	}
-
+	
 	@RequestMapping("/usr/member/doLogin")
 	@ResponseBody
 	public String doLogin(String loginId, String loginPw) {
-		if (rq.isLogined()) {
+		if ( rq.isLogined() ) {
 			return rq.jsHistoryBack("이미 로그인되었습니다.");
 		}
-
-		if (Ut.empty(loginId)) {
+		
+		if ( Ut.empty(loginId) ) {
 			return rq.jsHistoryBack("loginId(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(loginPw)) {
+		
+		if ( Ut.empty(loginPw) ) {
 			return rq.jsHistoryBack("loginPw(을)를 입력해주세요.");
 		}
-
+		
 		Member member = memberService.getMemberByLoginId(loginId);
-
-		if (member == null) {
+		
+		if ( member == null ) {
 			return rq.jsHistoryBack("존재하지 않은 로그인아이디 입니다.");
 		}
-
-		if (member.getLoginPw().equals(loginPw) == false) {
+		
+		if ( member.getLoginPw().equals(loginPw) == false ) {
 			return rq.jsHistoryBack("비밀번호가 일치하지 않습니다.");
 		}
-
+		
 		rq.login(member);
-
+		
 		return rq.jsReplace(Ut.f("%s님 환영합니다.", member.getNickname()), "/");
 	}
-
+	
 	@RequestMapping("/usr/member/getMembers")
 	@ResponseBody
 	public List<Member> getMembers() {
 		return memberService.getMembers();
 	}
-
+	
 	@RequestMapping("/usr/member/myPage")
-	public String showmyPage() {
-		return "/usr/member/myPage";
+	public String showMyPage() {
+		return "usr/member/myPage";
 	}
-
+	
 	@RequestMapping("/usr/member/checkPassword")
-	public String showcheckPassword() {
-		return "/usr/member/checkPassword";
+	public String showCheckPassword() {
+		return "usr/member/checkPassword";
 	}
-
+	
 	@RequestMapping("/usr/member/doCheckPassword")
+	@ResponseBody
 	public String doCheckPassword(String loginPw, String replaceUri) {
-		if (rq.getLoginedMember().getLoginPw().equals(loginPw) == false) {
+		if ( rq.getLoginedMember().getLoginPw().equals(loginPw) == false ) {
 			return rq.jsHistoryBack("비밀번호가 일치하지 않습니다.");
 		}
+		
 		return rq.jsReplace("", replaceUri);
 	}
-
+	
 	@RequestMapping("/usr/member/modify")
-	public String showmodify() {
-		return "/usr/member/modify";
+	public String showModify() {
+		return "usr/member/modify";
 	}
-
+	
 	@RequestMapping("/usr/member/doModify")
 	@ResponseBody
-	public String doModify(String loginId, String loginPw, String name, String nickname, String email,
-			String cellphoneNo) {
-		if (Ut.empty(loginPw)) {
+	public String doModify(String loginPw, String name, String nickname, String email, String cellphoneNo) {
+		if ( Ut.empty(loginPw) ) {
 			loginPw = null;
 		}
-		if (Ut.empty(name)) {
+		
+		if ( Ut.empty(name) ) {
 			return rq.jsHistoryBack("이름(을)를 입력해주세요.");
 		}
-		if (Ut.empty(nickname)) {
+		
+		if ( Ut.empty(nickname) ) {
 			return rq.jsHistoryBack("닉네임(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(email)) {
+		
+		if ( Ut.empty(email) ) {
 			return rq.jsHistoryBack("이메일(을)를 입력해주세요.");
 		}
-
-		if (Ut.empty(cellphoneNo)) {
+		
+		if ( Ut.empty(cellphoneNo) ) {
 			return rq.jsHistoryBack("휴대전화번호(을)를 입력해주세요.");
 		}
-
-		ResultData modifyRd = memberService.modify(rq.getLoginedMemberId(),loginPw,name,nickname,email,cellphoneNo);
-
-		return rq.jsReplace(modifyRd.getMsg(),"/");
+		
+		ResultData modifyRd = memberService.modify(rq.getLoginedMemberId(), loginPw, name, nickname, email, cellphoneNo);
+		
+		return rq.jsReplace(modifyRd.getMsg(), "/");
 	}
 }
